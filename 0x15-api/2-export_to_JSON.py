834c0a68ad5f9cce079a30313b18113478f3f556
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-04-22 23:27:41 +0100

    commit update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,7 +9,6 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    
     user_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(user_id)).json()
     todos = requests.get(url + "todos?userId={}".format(user_id)).json()
