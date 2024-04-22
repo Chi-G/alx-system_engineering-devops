82219ab9abe511250eca34257824680c99404486
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-04-22 23:30:35 +0100

    commit update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""exports to-do list information for a given employee ID to CSV format."""
 
 import csv
 import requests
 import sys
 
-
 if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-        user_id = sys.argv[1]
-        
-        user_response = requests.get(url + "users/{}".format(user_id))
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
 
-        user = user_response.json()
+    user_response = requests.get(url + "users/{}".format(user_id))
+    user = user_response.json()
+    username = user.get("username")
+    params = {"userId": user_id}
 
-        username = user.get("username")
+    todos_response = requests.get(url + "todos", params=params)
+    todos = todos_response.json()
 
-        params = {"userId": user_id}
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
-        todos_response = requests.get(url + "todos", params=params)
-
-        todos = todos_response.json()
-
-        with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
-                for todo in todos:
-                        writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+        for todo in todos:
+            writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
