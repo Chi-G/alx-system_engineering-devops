b6e0338713a5c62a4251ec7e89a25cb88bae806a
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-04-22 16:37:09 +0100

    commit update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,29 +7,23 @@ import sys
 
 
 if __name__ == "__main__":
-        # Get the user ID from the command-line arguments provided to the script
-            user_id = sys.argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        user_id = sys.argv[1]
+        
+        user_response = requests.get(url + "users/{}".format(user_id))
 
-                # Define the base URL for the JSON API
-                    url = "https://jsonplaceholder.typicode.com/"
+        user = user_response.json()
 
-                        # Fetch user information from the API and
-                            #   convert the response to a JSON object
-                                user = requests.get(url + "users/{}".format(user_id)).json()
+        username = user.get("username")
 
-                                    # Extract the username from the user data
-                                        username = user.get("username")
+        params = {"userId": user_id}
 
-                                            # Fetch the to-do list items associated with the
-                                                #   given user ID and convert the response to a JSON object
-                                                    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+        todos_response = requests.get(url + "todos", params=params)
 
-                                                        # Use list comprehension to iterate over the to-do list items
-                                                            # Write each item's details (user ID, username, completion status,
-                                                                #   and title) as a row in the CSV file
-                                                                    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-                                                                                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-                                                                                        [writer.writerow(
-                                                                                                        [user_id, username, t.get("completed"), t.get("title")]
-                                                                                                                 ) for t in todos]
+        todos = todos_response.json()
 
+        with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+                for todo in todos:
+                        writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
