c011b64092df6cd9a28f3d8e2588bae7096f9611
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-05-07 22:34:52 +0100

    commit update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,27 +8,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Retrieves the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit: A string representing the subreddit name.
-        
-    Returns:
-        The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
+    numbers of subscribers
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    res = requests.get(url, headers={'User-Agent': 'cap_keth'},
+                       allow_redirects=False)
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
     else:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-        sys.exit(1)
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
